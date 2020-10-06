84cb8edf118acd04916e7a9ec57b40dc8ed53c37
jackson kasi <71264394+jacksonkasi5@users.noreply.github.com>
2020-10-06 15:42:37 +0530

    hi
    hi

--- mail/mail.js
+++ mail/mail.js
@@ -8,20 +8,20 @@
 
 
           Host : "smtp.gmail.com",
-        //   Username : "mahebuddy952@gmail.com",
-        //   Password : "Mahe@123",
-        //   To : "mahebuddy952@gmail.com",
+           Username : "mahebuddy952@gmail.com",
+           Password : "Mahe@123",
+           To : "mahebuddy952@gmail.com",
 
-        Username : "nammalvar888@gmail.com",
-        Password : "AP.ta.1855*1855*",
-        To : "nammalvar888@gmail.com",
+       // Username : "nammalvar888@gmail.com",
+       // Password : "#########",
+        //To : "nammalvar888@gmail.com",
 
           From : document.getElementById('email').value,
           Subject : document.getElementById('subject').value,
           Body : document.getElementById('message').value
           }).then(function(response){ 
            if (response == 'OK') {              
-               alert("𝑀𝒶𝒾𝓁 𝓈𝑒𝓃𝓉 𝓈𝓊𝒸𝒸𝑒𝑒𝓈𝓈𝒻𝓊𝓁𝓁𝓎 ")
+               alert("𝑀𝒶𝒾𝓁 𝓈𝑒𝓃𝓉 𝓈𝓊𝒸𝒸𝑒𝑒𝓈𝓈𝒻𝓊𝓁𝓁𝓎 I Love U MAHI")
                ;
             } else {
                 throw new Error("Error: " + response.statusText);
